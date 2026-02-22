7df7bb2f2b1073d9f8d43f28e5eb0f7fb9a90652
yashabima2 <rivandio81@gmail.com>
2026-02-22 16:37:30 +0700

    "rev253"

--- app/customer/category/product/detail/lengkapipembelian/page.jsx
+++ app/customer/category/product/detail/lengkapipembelian/page.jsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { authFetch } from "@/lib/authFetch";
+import { authFetch } from "../../../../../lib/authFetch";
 
 export default function StepTwo() {
   const [checkout, setCheckout] = useState(null);
