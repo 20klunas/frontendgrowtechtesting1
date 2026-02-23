0072f5a3260ab411d8f95de48357d2bedaf229f9
yashabima2 <rivandio81@gmail.com>
2026-02-24 00:23:33 +0700

    "rev273"

--- app/customer/category/product/detail/lengkapipembelian/methodpayment/process/page.jsx
+++ app/customer/category/product/detail/lengkapipembelian/methodpayment/process/page.jsx
@@ -2,7 +2,7 @@
 
 import { Suspense, useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { authFetch } from "../../../../../../lib/authFetch";
+import { authFetch } from "../../../../../../../lib/authFetch";
 import { Lock, CheckCircle } from "lucide-react";
 
 function ProcessContent() {
