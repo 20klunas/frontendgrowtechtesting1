d235f7a6db03b80f43f13e9de59fb59b2681c3fe
yashabima2 <rivandio81@gmail.com>
2026-02-23 23:43:13 +0700

    "rev267"

--- app/customer/category/product/detail/lengkapipembelian/methodpayment/invoice/page.jsx
+++ app/customer/category/product/detail/lengkapipembelian/methodpayment/invoice/page.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import { authFetch } from "@/lib/authFetch";
+import { authFetch } from "../../../../../../../lib/authFetch";
 import {
   CheckCircle,
   Copy,
