8613c42c9871464459a40216804e74df7d2544e0
yashabima2 <rivandio81@gmail.com>
2026-03-06 17:31:19 +0700

    rev437

--- app/components/admin/PermissionGate.jsx
+++ app/components/admin/PermissionGate.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { usePermission } from "@/hooks/usePermission"
+import { usePermission } from "../../hooks/usePermission"
 
 export default function PermissionGate({ permission, children }) {
   const { can, loading } = usePermission()
