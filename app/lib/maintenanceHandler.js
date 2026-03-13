718e147e2bc4e5c406a6df49e3205de0c80a7de9
yashabima2 <rivandio81@gmail.com>
2026-03-13 15:53:26 +0700

    rev583

--- app/lib/maintenanceHandler.js
+++ app/lib/maintenanceHandler.js
@@ -2,6 +2,13 @@ export function handleMaintenance(res, data) {
 
   if (res.status === 503 && data?.meta?.maintenance) {
 
+    const pathname = window.location.pathname;
+
+    // Jangan redirect jika sedang di halaman OTP
+    if (pathname.startsWith("/verify-otp")) {
+      return;
+    }
+
     const message = encodeURIComponent(
       data?.error?.message || "System Maintenance"
     );
@@ -12,5 +19,4 @@ export function handleMaintenance(res, data) {
 
     throw new Error("System Maintenance");
   }
-
 }
